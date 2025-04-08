87a45375056a23d9a13146f357884d84fdc7b782
dkisantear <dkisantear@ecs-pa-coding1.ecs.csus.edu>
2025-04-08 11:12:03 -0700

    v4

--- MyLowPower.h
+++ MyLowPower.h
@@ -1,12 +1,21 @@
 #ifndef MY_LOW_POWER_H
 #define MY_LOW_POWER_H
 
-namespace MyLowPower {
-  const int SLEEP_250MS = 0;
-  const int ADC_OFF = 1;
-  const int BOD_OFF = 2;
+#include <avr/sleep.h>
+#include <avr/power.h>
 
-  void powerDown(int sleepMode, int adcSetting, int bodSetting);
-}
+class MyLowPower {
+public:
+    static const int SLEEP_250MS = 0;
+    static const int ADC_OFF = 0;
+    static const int BOD_OFF = 0;
+
+    static void powerDown(int sleepTime, int adc, int bod) {
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_enable();
+        sleep_mode();
+        sleep_disable();
+    }
+};
 
 #endif
